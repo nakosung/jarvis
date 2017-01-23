293bb7fdb7c8cb5887110e3c3e55232d65a3ef3a
Nako Sung <deif79@gmail.com>
2017-01-24 01:04:28 +0900

    ...

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function gather_vars(recipe) {
                 } else {
                     out[k] = ['range', v]
                 }                
-            } else if (_.isNumber(v)) {
+            } else if (_.isString(v) || _.isNumber(v)) {
                 out[k] = ['const', v]
             } else if (_.isObject(v)) {
                 let sub = gather_vars(v)
@@ -88,24 +88,70 @@ function sample(recipe) {
 
 function locate(data,vars) {
     if (data["@origin"] == undefined) throw new Error("Unable to locate non-data")
-    return launch_with_vars(data,vars)
+    return launch_with_vars(data,vars).then(([node,cont]) => {
+        let docker = new Docker(node)
+        return docker.exec(`wait ${cont}`).then(() => [[node,cont]])
+    })
+}
+
+function scp(src_host,src_file,dest_host,dest_file) {
+    return Promise.resolve()
+}
+
+function move(recipe,vars,src,dest) {
+    let src_docker = new Docker(src[0])
+    let dest_docker = new Docker(dest[1])
+    let id = src[1]
+    let dest_id = dest[1]
+    return src_docker.exec(`run --rm --volumes-from ${id} -v /tmp:/backup ubuntu tar cvf /backup/${id} /dbdata-fixme`)
+        .then(() => scp(src[0],`/tmp/${id}.tar`,dest[1],`/tmp/${id}.tar`))
+        .then(() => dest_docker.exec(`run -v /dbdata-fix-me --name ${dest_id} ubuntu /bin/bash`))
+        .then(() => dest_docker.exec(`run --volumes-from ${dest_id} -v /tmp:/backup ubuntu bash -c "cd /dbdata && tar xvf /backup/${id}.tar --strip 1"`))    
+}
+
+function calc_id(recipe,vars) {
+    return Math.random().toString(16).substr(2,12)
 }
 
 function launch_with_vars(recipe,vars) {
     let pre = []
+    let Ks = []
     _.each(recipe,(v,k) => {
         if (v["@origin"]) {
-            pre.push(locate(v,vars[k]))
+            pre.push(locate(v,vars[k] || {}))
+            Ks.push(k)
         }
     })
-    return Promise.all(pre).then(() => {
-        console.log(`Launching ${JSON.stringify(recipe)}`)
+    return Promise.all(pre).then(locations => {        
+        let node = vars.node || (locations.length ? locations[0][0][0] : '?')
+        let moves = []
+        locations.forEach((v,k) => {
+            k = Ks[k]
+            if (!_.some(v,v => v[0] == node)) {            
+                let first = v[0]    
+                moves.push(move(recipe[k],vars[k],first,[node,first[1]]))
+            }        
+        })
+        return Promise.all(moves).then(() => {
+            console.log(`Launching @${node} ${JSON.stringify(recipe).substr(0,60)}`)
+            let docker = new Docker(node)            
+            let image = recipe["@image"]
+            let volumes = locations.map(l => l[0][1])
+            let id = image + '-' + calc_id(recipe,vars)
+            volumes = volumes.map(v => `--volumes-from ${v}`).join(' ')
+            return docker.exec(`build -t ${image}`).then(x => 
+                docker.exec(`run -t ${volumes} --name ${id} ${image}`)
+            ).then(x => [node,id])
+        })
     })
 }
 
 function launch(recipe) {
     return sample(recipe).then(vars => launch_with_vars(recipe,vars))
 }
+
+// runner가 reporter와 함께 작동한다. (working container -> report)
+// master는 runner의 상태를 모두 monitoring한다.
 
 class Docker {
     constructor(host) {
@@ -126,25 +172,9 @@ class Container {
     }
 }
 
-function volumes_from_recipe(recipe) {
-    let volumes = []
-    _.each(recipe,(v,k) => {
-        if (_.isObject(v) && v["@origin"]) {
-            volumes.push(k)
-        }
-    })
-    return volumes
-}
-
 class Instance extends Container {
     constructor(recipe,id) {
         super(recipe,id)
-    }
-
-    run() {
-        let volumes = volumes_from_recipe(recipe)
-        volumes = volumes.map(v => `--volumes-from ${v}`).join(' ')
-        return this.docker.exec(`run --name ${this.instance} ${volumes}`)
     }
 
     freeze() {
@@ -161,9 +191,3 @@ function search(recipe) {
 }
 
 launch(recipe)
-new Instance(recipe,"test@your-node").run()
-search(recipe).then(instances => {
-    instances.forEach(x => {
-        x.freeze().then(_ => x.thaw())
-    })
-})
