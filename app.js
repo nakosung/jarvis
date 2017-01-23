1bd9ad4ef87d7a31f8227a1b9e96011fb1fcd315
Nako Sung <nakosung@ncsoft.com>
2017-01-23 18:32:57 +0900

    ..

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+const _ = require('lodash')
+
 let recipe = {
-    "github.com/nakosung/trainer": {
-        "data" : {
-            "github.com/nakosung/processor": {
-                // 하나면 constant
-                "args": ["--datadir /data --outdir $out"],
-                "data": {
-                    "github.com/nakosung/data-source": {
-                        "args": "--outdir $out"
-                    }
-                },
-                normalize: [true,false]                
+    "origin": "github.com/nakosung/trainer",
+    "data" : {
+        "origin": "github.com/nakosung/processor",
+        // 하나면 constant
+        "args": ["--datadir /data --outdir $out"],
+        "data": {
+            "github.com/nakosung/data-source": {
+                "args": "--outdir $out"
             }
         },
-        "iter": 3000,
-        // array는 enumerate
-        "dropout": [true,false],
-        // 추가 정보는 마지막에
-        "learning-rate": [0.2,0.5,{cont:'log'}]
-    }
+        normalize: [true,false]
+    },
+    "iter": 3000,
+    // array는 enumerate
+    "dropout": [true,false],
+    // 추가 정보는 마지막에
+    "learning-rate": [0.2,0.5,{cont:'log'}]
 }
 
 function launch(recipe) {
@@ -43,9 +43,25 @@ class Container {
     }
 }
 
+function volumes_from_recipe(recipe) {
+    let volumes = []
+    _.each(recipe,(v,k) => {
+        if (_.isObject(v) && v.origin) {
+            volumes.push(k)
+        }
+    })
+    return volumes
+}
+
 class Instance extends Container {
     constructor(recipe,id) {
         super(recipe,id)
+    }
+
+    run() {
+        let volumes = volumes_from_recipe(recipe)
+        volumes = volumes.map(v => `--volumes-from ${v}`).join(' ')
+        return this.docker.exec(`run --name ${this.instance} ${volumes}`)
     }
 
     freeze() {
@@ -62,6 +78,7 @@ function search(recipe) {
 }
 
 launch(recipe)
+new Instance(recipe,"test@your-node").run()
 search(recipe).then(instances => {
     instances.forEach(x => {
         x.freeze().then(_ => x.thaw())
